39946ed36d1c77912b3b38699da0d4052b9fea0a
Alexandr Ivanov <alexandr.sky@gmail.com>
2015-03-30 12:28:34 +0300

    cnt to u

--- esp8266_server.py
+++ esp8266_server.py
@@ -3,9 +3,12 @@
 import socket, time
 
 
-addr = ('192.168.0.66', 8266)
+addr = ('192.168.8.66', 8266)
 DEEP_SLEEP_TIME = 60
 MAX_CONNECTION_ATTEMPTS = 20
+
+
+acc_table = [(682, 2.93), (691, 2.91), (694, 2.9), (696, 2.89), (700, 2.88), (720, 2.84), (727, 2.81), (737, 2.79), (740, 2.78), (745, 2.77), (750, 2.76), (759, 2.74), (763, 2.73), (780, 2.69), (786, 2.68), (796, 2.65), (800, 2.64), (808, 2.63), (815, 2.62), (828, 2.6), (833, 2.59), (840, 2.58), (846, 2.57), (853, 2.56), (858, 2.55), (865, 2.54), (872, 2.53), (880, 2.52), (886, 2.51), (894, 2.5), (932, 2.44), (973, 2.41), (1021, 2.35), (1031, 2.34), (1042, 2.33), (1050, 2.32)]
 
 
 def calc_sht(raw_temp, raw_hum):
@@ -26,6 +29,34 @@ def calc_sht(raw_temp, raw_hum):
 	hum = (temp - 25) * (T1 + T2 * raw_hum) + lin_hum
 
 	return temp, hum
+
+def cnt2u(cnt):
+	c1, c2 = 0, 0
+	u1, u2 = 0, 0
+	for c, u in acc_table:
+		if c < cnt:
+			c1 = c
+			u1 = u
+		elif c == cnt:
+			return u
+		elif c > cnt:
+			c2 = c
+			u2 = u
+			break
+
+	if not (c1 or c2):
+		return 0
+
+	if not c2:
+		c1, u1 = acc_table[-2]
+		c2, u2 = acc_table[-1]
+	elif not c1:
+		c1, u1 = acc_table[0]
+		c2, u2 = acc_table[1]
+
+	du = (u2-u1)/(c2-c1)
+
+	return (float(cnt) - c1)*du + u1
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -51,7 +82,8 @@ while True:
 	temp, hum = calc_sht(temp, hum)
 	btemp = float(btemp)
 
-	battery, ds_time, max_conn, conn_time = int(battery), int(ds_time), int(max_conn), int(conn_time)
+	battery = cnt2u(battery)
+	ds_time, max_conn, conn_time = int(ds_time), int(max_conn), int(conn_time)
 
 	print '%d) %s T=%.2f, H=%.2f%%, BT=%.2f (bat=%d, ds=%d, mc=%d, ct=%d)' % (cnt, time.strftime("%H:%M:%S"), temp, hum, btemp, battery, ds_time, max_conn, conn_time)
 
